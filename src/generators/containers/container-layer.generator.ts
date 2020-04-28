907649780419d5876edb72cc3eb1e7618e606b9a
Matt Mackay <mattem@gmail.com>
2020-04-28 19:03:18 -0400

    feat: add simple container_layer generation (#38)

---
+++ src/generators/containers/container-layer.generator.ts
@@ -0,0 +1,32 @@
+import { GeneratorType } from '../../flags';
+import { BuildFileGenerator } from '../generator';
+
+export class ContainerLayerGenerator extends BuildFileGenerator {
+
+  async generate(): Promise<void> {
+    const label = this.workspace.getLabelForPath().withTarget('layer');
+
+    this.buildozer.loadRule('container_layer', label);
+    this.buildozer.newRule('container_layer', label);
+
+    const files = [];
+    if (this.workspace.isDirectory()) {
+      let labels = this.workspace.readDirectory()
+        .map(file => this.workspace.getFileLabel(file));
+
+      files.push(...labels);
+    } else {
+      files.push(this.workspace.getFileLabel(this.flags.path));
+    }
+
+    this.buildozer.addAttr('files', files, label);
+  }
+
+  getGeneratorType(): GeneratorType {
+    return GeneratorType.CONTAINER_LAYER;
+  }
+
+  supportsDirectories(): boolean {
+    return true;
+  }
+}
